bad3ae38a76aeb7a48fbe27a49a31420febd7526
andrapratama <andrasatriapratama@gmail.com>
2017-07-12 11:56:54 +0700

    versi latihan

---
+++ app/src/main/java/com/androidalertdialogr/example/MainActivity.java
@@ -0,0 +1,49 @@
+package com.androidalertdialogr.example;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        onButtonClickListener();
+    }
+
+    public void onButtonClickListener() {
+        btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        AlertDialog.Builder a_builder = new AlertDialog.Builder(MainActivity.this);
+                        a_builder.setMessage("Apakah anda ingin keluar dari aplikasi?")
+                                .setCancelable(false)
+                                .setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        finish();
+                                    }
+                                })
+                                .setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.cancel();
+                                    }
+                                });
+                        AlertDialog alert = a_builder.create();
+                        alert.setTitle("Perhatian !");
+                        alert.show();
+                    }
+                }
+        );
+    }
+}
